15182ebd08dd23d98341f4ddc755d6d73732875e
jbgrocha <jose.bernardo.rocha2@gmail.com>
2015-04-20 23:24:11 +0100

    added createCouchDB.js

---
+++ learnyoucouchdb/createCouchDB.js
@@ -0,0 +1,26 @@
+var http = require('http')
+var request = require('request')
+
+var couchdbPath = 'http://localhost:5984/'
+var dbName = process.argv[2]
+
+var h = {
+  accept: 'application/json',
+  'content-type': 'application/json'
+}
+
+var options = {
+  uri: couchdbPath + dbName,
+  method:'PUT',
+  headers:h
+}
+
+function callback(err, response, body) {
+  if (err)
+    throw err;
+  if (response.statusCode !== 201)
+    throw new Error("Could not create database. " + body);
+}
+
+
+request(options, callback).end()
